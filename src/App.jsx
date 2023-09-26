83802c69ab1cd3289672639ed4081e7d37b84d1f
Breno Ocanha <breno@dmxdesign.com.br>
2023-09-26 08:54:51 -0300

    fix: build

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Router } from "./router";
+import { Router } from "./router.jsx";
 import { BrowserRouter } from "react-router-dom";
 import { Header } from "./components/Header";
 import { Footer } from "./components/Footer";
